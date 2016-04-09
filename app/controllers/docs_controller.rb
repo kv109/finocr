f199da424cdbb6da34c84b86072db179c59a3bbd
Kacper Walanus <kacper@walanus.com>
2016-04-09 11:34:41 +0200

    convert sent pdf to text

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,4 +3,15 @@ class DocsController < ApplicationController
   def new
   end
 
+  def create
+    reader = PDF::Reader.new(params[:file].tempfile)
+
+    pages = []
+    reader.pages.each do |page|
+      pages << page.text
+    end
+
+    render plain: pages.first
+  end
+
 end
